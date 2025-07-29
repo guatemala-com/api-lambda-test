affcf9b2b34ecabd136fd9e215b446c2079ad3cb
Cesar Monsalvo <cesamone@sumerce.guru>
2025-07-29 18:21:26 -0500

    Update app.service.ts
    El método getHello() devuelve un objeto { message: 'Hello World!' }. Sin embargo, tus pruebas E2E (test/app.e2e-spec.ts) y unitarias (src/app.controller.spec.ts) esperan una cadena de texto simple: 'Hello World!'.
    
    Solución: Modificaremos el método para que devuelva directamente la cadena de texto.

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    return {
-      message: 'Hello World!',
-    };
+  getHello(): string {
+    return 'Hello World!';
   }
 }
